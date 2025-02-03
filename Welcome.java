95ba66546366608d8e412c3b01b767b4a6c17d25
Dahlia <embracedahlia@gmail.com>
2025-02-03 15:27:35 -0600

    chores: Added welcome 3 message

--- Welcome.java
+++ Welcome.java
@@ -2,5 +2,6 @@ public class Welcome{
     public static void main(String[] args){
         System.out.println("Welcome!");
         System.out.println("Welcome next.");
+        System.out.println("Welcome 3.");
     }
 }
